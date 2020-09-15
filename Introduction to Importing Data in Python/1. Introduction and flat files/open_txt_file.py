1b83693a0a2ace3a4ae81a2dbb2e7b3ada00efb7
Joao Lima <joaoplima@ua.pt>
2020-09-15 17:50:07 +0200

    open txt file

---
+++ Introduction to Importing Data in Python/1. Introduction and flat files/open_txt_file.py
@@ -0,0 +1,22 @@
+# Importing entire text files
+# Open a file: file
+file = open('moby_dick.txt', 'r')
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
+
+# Importing text files line by line
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
