d05d82b19b59c1e7d50fc4c26f86eee98fda9096
Joao Lima <joaoplima@ua.pt>
2020-09-16 16:45:13 +0200

    Query DB

---
+++ Introduction to Importing Data in Python/3. Working with relational databases in Python/query_db.py
@@ -0,0 +1,67 @@
+# The Hello World of SQL Queries!
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine connection: con
+con = engine.connect()
+
+# Perform query: rs
+rs = con.execute('SELECT * FROM Album')
+
+# Save results of the query to DataFrame: df
+df = pd.DataFrame(rs)
+
+# Close connection
+con.close()
+
+# Print head of DataFrame df
+print(df.head())
+
+
+# Customizing the Hello World of SQL Queries
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT LastName, Title FROM Employee')
+    df = pd.DataFrame(rs.fetchmany(size=3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Filtering your database records using SQL's WHERE
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee WHERE EmployeeId >= 6')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Ordering your SQL records with ORDER BY
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee ORDER BY BirthDate')
+    df = pd.DataFrame(rs.fetchall())
+
+    # Set the DataFrame's column names
+    df.columns = rs.keys()
+
+
+# Print head of DataFrame
+print(df.head())
