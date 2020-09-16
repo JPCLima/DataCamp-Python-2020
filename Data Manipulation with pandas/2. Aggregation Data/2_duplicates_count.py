8135397fc5d2afb3334ea641c0c5def536645e1b
Joao Lima <joaoplima@ua.pt>
2020-09-16 18:29:32 +0200

    dduplicates and count

---
+++ Data Manipulation with pandas/2. Aggregation Data/2_duplicates_count.py
@@ -0,0 +1,32 @@
+# Dropping duplicates
+# Drop duplicate store/type combinations
+store_types = sales.drop_duplicates(subset=["store", "type"])
+print(store_types.head())
+
+# Drop duplicate store/department combinations
+store_depts = sales.drop_duplicates(subset=["store", "department"])
+print(store_depts.head())
+
+# Subset the rows where is_holiday is True and drop duplicate dates
+holiday_dates = sales[sales["is_holiday"]].drop_duplicates(subset="date")
+
+# Print date col of holiday_dates
+print(holiday_dates["date"])
+
+# Counting categorical variables
+# Count the number of stores of each type
+store_counts = store_types['type'].value_counts()
+print(store_counts)
+
+# Get the proportion of stores of each type
+store_props = store_types['type'].value_counts(normalize=True)
+print(store_props)
+
+# Count the number of each department number and sort
+dept_counts_sorted = store_depts['department'].value_counts(sort=True)
+print(dept_counts_sorted)
+
+# Get the proportion of departments of each number and sort
+dept_props_sorted = store_depts['department'].value_counts(
+    sort='departments', normalize=True)
+print(dept_props_sorted)
