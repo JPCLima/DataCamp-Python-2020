51c5d59d88dfb8a6c47a48ed60c0e2b22ede1f98
Joao Lima <joaoplima@ua.pt>
2020-09-16 16:13:22 +0200

    SAS ad Stata

---
+++ Introduction to Importing Data in Python/2. Importing data from other file types/sas_Stata.py
@@ -0,0 +1,34 @@
+# Importing SAS files
+# Import sas7bdat package
+import pandas as pd
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+# Using read_stata to import Stata files
+df = pd.read_stata('disarea.dta')
+
+# Importing Stata files
+# Import pandas
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
