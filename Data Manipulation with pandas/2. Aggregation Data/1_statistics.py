f648aa05f2572001f9015abc5bb7096ec8e6bcfc
Joao Lima <joaoplima@ua.pt>
2020-09-16 18:17:12 +0200

    statistics

---
+++ Data Manipulation with pandas/2. Aggregation Data/1_statistics.py
@@ -0,0 +1,45 @@
+# Mean and median
+# Print the head of the sales DataFrame
+import numpy as np
+print(sales.head())
+
+# Print the info about the sales DataFrame
+print(sales.info())
+
+# Print the mean of weekly_sales
+print(sales.weekly_sales.mean())
+
+# Print the median of weekly_sales
+print(sales.weekly_sales.median())
+
+# Summarizing dates
+# Print the maximum of the date column
+print(sales.date.max())
+
+# Print the minimum of the date column
+print(sales.date.min())
+
+# Efficient summaries
+# Import NumPy and create custom IQR function
+
+
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l",
+             "unemployment"]].agg([iqr, np.median]))
+
+# Cumulative statistics
+# Sort sales_1_1 by date
+sales_1_1 = sales_1_1.sort_values('date')
+
+# Get the cumulative sum of weekly_sales, add as cum_weekly_sales col
+sales_1_1['cum_weekly_sales'] = sales_1_1['weekly_sales'].cumsum()
+
+# Get the cumulative max of weekly_sales, add as cum_max_sales col
+sales_1_1['cum_max_sales'] = sales_1_1['weekly_sales'].cummax()
+
+# See the columns you calculated
+print(sales_1_1[["date", "weekly_sales", "cum_weekly_sales", "cum_max_sales"]])
