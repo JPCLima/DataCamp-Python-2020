4dbdfebc71964fc424dc74a47e922ec4bc9cf9fb
Joao Lima <joaoplima@ua.pt>
2020-09-16 16:24:20 +0200

    import HDF5 and matlab files

---
+++ Introduction to Importing Data in Python/2. Importing data from other file types/import_matlab.py
@@ -0,0 +1,28 @@
+# Loading .mat files
+# Import package
+import scipy.io
+
+# Load MATLAB file: mat
+mat = scipy.io.loadmat('albeck_gene_expression.mat')
+
+# Print the datatype type of mat
+print(type(mat))
+
+
+# The structure of .mat in Python
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['CYratioCyt']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(mat['CYratioCyt'].shape)
+
+# Subset the array and plot it
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
