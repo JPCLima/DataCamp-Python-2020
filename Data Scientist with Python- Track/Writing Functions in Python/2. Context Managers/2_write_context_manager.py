9c0ff50c1d9351a07a1c1ace2ee02cd4ca1da1f0
Joao Lima <joaoplima@ua.pt>
2020-09-23 19:40:13 +0200

    write context manager

---
+++ Data Scientist with Python- Track/Writing Functions in Python/2. Context Managers/2_write_context_manager.py
@@ -0,0 +1,42 @@
+# The timer() context manager
+# Add a decorator that will make timer() a context manager
+@contextlib.contextmanager
+def timer():
+    """Time the execution of a context block.
+
+    Yields:
+      None
+    """
+    start = time.time()
+    # Send control back to the context block
+    yield
+    end = time.time()
+    print('Elapsed: {:.2f}s'.format(end - start))
+
+
+with timer():
+    print('This should take approximately 0.25 seconds')
+    time.sleep(0.25)
+
+# A read-only open() context manager
+
+
+@contextlib.contextmanager
+def open_read_only(filename):
+    """Open a file in read-only mode.
+
+    Args:
+      filename (str): The location of the file to read
+
+    Yields:
+      file object
+    """
+    read_only_file = open(filename, mode='r')
+    # Yield read_only_file so it can be assigned to my_file
+    yield read_only_file
+    # Close read_only_file
+    read_only_file.close()
+
+
+with open_read_only('my_file.txt') as my_file:
+    print(my_file.read())
