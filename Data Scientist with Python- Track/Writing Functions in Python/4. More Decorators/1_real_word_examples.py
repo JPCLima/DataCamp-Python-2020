c5762748ffe8f1ab6ef1f87ffb117b8ebc9ea05e
Joao Lima <joaoplima@ua.pt>
2020-09-24 15:36:37 +0200

    practice decorators

---
+++ Data Scientist with Python- Track/Writing Functions in Python/4. More Decorators/1_real_word_examples.py
@@ -0,0 +1,47 @@
+# Print the return type
+def print_return_type(func):
+    # Define wrapper(), the decorated function
+    def wrapper(*args, **kwargs):
+        # Call the function being decorated
+        result = func(*args, **kwargs)
+        print('{}() returned type {}'.format(
+            func.__name__, type(result)
+        ))
+        return result
+    # Return the decorated function
+    return wrapper
+
+
+@print_return_type
+def foo(value):
+    return value
+
+
+print(foo(42))
+print(foo([1, 2, 3]))
+print(foo({'a': 42}))
+
+# Counter
+
+
+def counter(func):
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        # Call the function being decorated and return the result
+        return func(*args, **kwargs)
+    wrapper.count = 0
+    # Return the new decorated function
+    return wrapper
+
+# Decorate foo() with the counter() decorator
+
+
+@counter
+def foo():
+    print('calling foo()')
+
+
+foo()
+foo()
+
+print('foo() was called {} times.'.format(foo.count))
