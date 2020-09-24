8705c4f47a2b8031caabdf7988ff857d9cddc317
Joao Lima <joaoplima@ua.pt>
2020-09-24 15:29:56 +0200

    decorators

---
+++ Data Scientist with Python- Track/Writing Functions in Python/3. Decorators/4_decorators.py
@@ -0,0 +1,29 @@
+# Using decorator syntax
+def my_function(a, b, c):
+    print(a + b + c)
+
+
+# Decorate my_function() with the print_args() decorator
+my_function = print_args(my_function)
+
+my_function(1, 2, 3)
+
+# Defining a decorator
+
+
+def print_before_and_after(func):
+    def wrapper(*args):
+        print('Before {}'.format(func.__name__))
+        # Call the function being decorated with *args
+        func(*args)
+        print('After {}'.format(func.__name__))
+    # Return the nested function
+    return wrapper
+
+
+@print_before_and_after
+def multiply(a, b):
+    print(a * b)
+
+
+multiply(5, 10)
