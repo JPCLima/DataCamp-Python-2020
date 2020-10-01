d4650c4089460d7da72e2ce1bce7c1d2140e2855
Joao Lima <joaoplima@ua.pt>
2020-10-01 10:48:18 +0200

    crosstab

---
+++ Machine Learning Scientist with Python - Track/Unsupervised Learning in Python/1. Clustering for dataset exploration/crosstab_clustering.py
@@ -0,0 +1,37 @@
+# How many clusters of grain?
+ks = range(1, 6)
+inertias = []
+
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)
+
+    # Fit model to samples
+    model.fit(samples)
+
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+
+# Plot ks vs inertias
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
+
+
+# Evaluating the grain clustering
+# Create a KMeans model with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Use fit_predict to fit model and obtain cluster labels: labels
+labels = model.fit_predict(samples)
+
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+
+# Create crosstab: ct
+ct = pd.crosstab(df.labels, df.varieties)
+
+# Display ct
+print(ct)
