317c7b5c702929779f627b683789e2807561ca47
Joao Lima <joaoplima@ua.pt>
2020-10-04 16:29:08 +0200

    Date and time

---
+++ Data Scientist with Python- Track/Working with Dates and Times in Python/2. Combining Dates and Times/1_date_time.py
@@ -0,0 +1,45 @@
+# Creating datetimes by hand
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 10, 1, 15, 26, 26)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
+
+# Import datetime
+
+# Create a datetime object
+dt = datetime(year=2017, month=12, day=31, hour=15, minute=19, second=13)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
+
+# Import datetime
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Replace the year with 1917
+dt_old = dt.replace(year=1917)
+
+# Print the results in ISO 8601 format
+print(dt_old)
+
+
+# Counting events before and after noon
+# Create dictionary to hold results
+trip_counts = {'AM': 0, 'PM': 0}
+
+# Loop over all trips
+for trip in onebike_datetimes:
+    # Check to see if the trip starts before noon
+    if trip['start'].hour < 12:
+        # Increment the counter for before noon
+        trip_counts['AM'] += 1
+    else:
+        # Increment the counter for after noon
+        trip_counts['PM'] += 1
+
+print(trip_counts)
