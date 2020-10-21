af39484926bbb87b7b17dde6e758b27663888ef5
Joao Lima <joaoplima@ua.pt>
2020-10-21 13:01:34 +0200

    ending Daylight

---
+++ Data Scientist with Python- Track/Working with Dates and Times in Python/3. Timme Zones and Daylight Saving/endindDaylight.py
@@ -0,0 +1,26 @@
+# Finding ambiguous datetimes
+# Loop over trips
+for trip in onebike_datetimes:
+    # Rides with ambiguous start
+    if tz.datetime_ambiguous(trip['start']):
+        print("Ambiguous start at " + str(trip['start']))
+    # Rides with ambiguous end
+    if tz.datetime_ambiguous(trip['end']):
+        print("Ambiguous end at " + str(trip['end']))
+
+# Cleaning daylight saving data with fold
+trip_durations = []
+for trip in onebike_datetimes:
+    # When the start is later than the end, set the fold to be 1
+    if trip['start'] > trip['end']:
+        trip['end'] = tz.enfold(trip['end'])
+    # Convert to UTC
+    start = trip['start'].astimezone(tz.UTC)
+    end = trip['end'].astimezone(tz.UTC)
+
+    # Subtract the difference
+    trip_length_seconds = (end-start).total_seconds()
+    trip_durations.append(trip_length_seconds)
+
+# Take the shortest trip duration
+print("Shortest trip: " + str(min(trip_durations)))
