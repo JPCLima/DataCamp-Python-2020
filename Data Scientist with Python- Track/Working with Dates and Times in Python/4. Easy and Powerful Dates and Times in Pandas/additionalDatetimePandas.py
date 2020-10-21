4cd61b68d441a1b43523bee23b51b5b86af9d6cd
Joao Lima <joaoplima@ua.pt>
2020-10-21 13:24:26 +0200

    Additional Datetime in pandas

---
+++ Data Scientist with Python- Track/Working with Dates and Times in Python/4. Easy and Powerful Dates and Times in Pandas/additionalDatetimePandas.py
@@ -0,0 +1,33 @@
+# Timezones in Pandas
+# Localize the Start date column to America/New_York
+rides['Start date'] = rides['Start date'].dt.tz_localize('America/New_York',
+                                                         ambiguous='NaT')
+
+# Print first value
+print(rides['Start date'].iloc[0])
+
+# Convert the Start date column to Europe/London
+rides['Start date'] = rides['Start date'].dt.tz_convert('Europe/London')
+
+# Print the new value
+print(rides['Start date'].iloc[0])
+
+# How long per weekday?
+# Add a column for the weekday of the start of the ride
+rides['Ride start weekday'] = rides['Start date'].dt.weekday_name
+
+# Print the median trip time per weekday
+print(rides.groupby('Ride start weekday')['Duration'].median())
+
+# How long between rides?
+# Shift the index of the end date up one; now subract it from the start date
+rides['Time since'] = rides['Start date'] - (rides['End date'].shift(1))
+
+# Move from a timedelta to a number of seconds, which is easier to work with
+rides['Time since'] = rides['Time since'].dt.total_seconds()
+
+# Resample to the month
+monthly = rides.resample('M', on='Start date')
+
+# Print the average hours between rides each month
+print(monthly['Time since'].mean()/(60*60))
