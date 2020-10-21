4bf12b5709b9f4f10ecc6d69404cba88ab725884
Joao Lima <joaoplima@ua.pt>
2020-10-21 13:08:55 +0200

    Read dates in pandas

---
+++ Data Scientist with Python- Track/Working with Dates and Times in Python/4. Easy and Powerful Dates and Times in Pandas/readingDatesFromPandas.py
@@ -0,0 +1,19 @@
+# Loading a csv file in Pandas
+# Import pandas
+import pandas as pd
+
+# Load CSV into the rides variable
+rides = pd.read_csv('capital-onebike.csv',
+                    parse_dates=['Start date', 'End date'])
+
+# Print the initial (0th) row
+print(rides.iloc[0])
+
+# Making timedelta columns
+# Subtract the start date from the end date
+ride_durations = rides['End date'] - rides['Start date']
+
+# Convert the results to seconds
+rides['Duration'] = ride_durations.dt.total_seconds()
+
+print(rides['Duration'].head())
