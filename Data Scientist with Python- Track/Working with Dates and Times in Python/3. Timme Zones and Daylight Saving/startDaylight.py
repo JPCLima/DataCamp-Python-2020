8735a594cd41b624dec5b7d764507de3ef71a948
Joao Lima <joaoplima@ua.pt>
2020-10-21 13:00:12 +0200

    Daylight saving time

---
+++ Data Scientist with Python- Track/Working with Dates and Times in Python/3. Timme Zones and Daylight Saving/startDaylight.py
@@ -0,0 +1,27 @@
+# How many hours elapsed around daylight saving?
+# Import datetime, timedelta, tz, timezone
+from datetime import datetime
+from datetime import datetime, timedelta, timezone
+from dateutil import tz
+
+# Start on March 12, 2017, midnight, then add 6 hours
+start = datetime(2017, 3, 12, tzinfo=tz.gettz('America/New_York'))
+end = start + timedelta(hours=6)
+print(start.isoformat() + " to " + end.isoformat())
+
+# How many hours have elapsed?
+print((end - start).total_seconds()/(60*60))
+
+# What if we move to UTC?
+print((end.astimezone(timezone.utc) - start.astimezone(timezone.utc))
+      .total_seconds()/(60*60))
+
+# March 29, throughout a decade
+# Import datetime and tz
+
+# Create starting date
+dt = datetime(2000, 3, 29, tzinfo=tz.gettz('Europe/London'))
+
+# Loop over the dates, replacing the year, and print the ISO timestamp
+for y in range(2000, 2011):
+    print(dt.replace(year=y).isoformat())
