9ea0cd44c3e21f2ebf6a204e69676b266c986602
Joao Lima <joaoplima@ua.pt>
2020-10-21 12:56:44 +0200

    timezone database

---
+++ Data Scientist with Python- Track/Working with Dates and Times in Python/3. Timme Zones and Daylight Saving/timezoneDatabse.py
@@ -0,0 +1,52 @@
+# Putting the bike trips into the right time zone
+# Import tz
+from dateutil import tz
+
+# Create a timezone object for Eastern Time
+et = tz.gettz('America/New_York')
+
+# Loop over trips, updating the datetimes to be in Eastern Time
+for trip in onebike_datetimes[:10]:
+    # Update trip['start'] and trip['end']
+    trip['start'] = trip['start'].replace(tzinfo=et)
+    trip['end'] = trip['end'].replace(tzinfo=et)
+
+# What time did the bike leave? (Global edition)
+# Create the timezone object
+uk = tz.gettz('Europe/London')
+
+# Pull out the start of the first trip
+local = onebike_datetimes[0]['start']
+
+# What time was it in the UK?
+notlocal = local.astimezone(uk)
+
+# Print them out and see the difference
+print(local.isoformat())
+print(notlocal.isoformat())
+
+# Create the timezone object
+ist = tz.gettz('Asia/Kolkata')
+
+# Pull out the start of the first trip
+local = onebike_datetimes[0]['start']
+
+# What time was it in the UK?
+notlocal = local.astimezone(ist)
+
+# Print them out and see the difference
+print(local.isoformat())
+print(notlocal.isoformat())
+
+# Create the timezone object
+sm = tz.gettz('Pacific/Apia')
+
+# Pull out the start of the first trip
+local = onebike_datetimes[0]['start']
+
+# What time was it in Samoa?
+notlocal = local.astimezone(sm)
+
+# Print them out and see the difference
+print(local.isoformat())
+print(notlocal.isoformat())
